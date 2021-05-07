31e022a364199f0a74f39046c4ae81f2c2daf9b6
Washington Bezerra <luiswashington00@gmail.com>
2021-05-06 22:57:38 -0300

    Mudança na nome do App

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:carteiradigital/main.dart';
+import 'package:heyparty/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
